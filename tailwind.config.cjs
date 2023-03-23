81f1c1b60db1c70a25e6fbe23d22d250879d2958
pyakz <markvergelbanguis@gmail.com>
2023-03-23 13:39:49 +0800

    chore: setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {
-        // brand_dark: "#000000",
-        // brand_light: "#FFFFFF",
-        // brand_primary: "##1A1C1E",
-        // brand_secondary: "#313334",
-        // brand_tertiary: "#4F5052",
-      },
+      colors: {},
     },
   },
   plugins: [],
