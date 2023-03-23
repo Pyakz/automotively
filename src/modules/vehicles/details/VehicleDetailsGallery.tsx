041510e1c1c1cfd0e15c292daf763130676b3b59
pyakz <markvergelbanguis@gmail.com>
2023-03-23 12:48:43 +0800

    feat: VehicleDetailsGallery

--- src/modules/vehicles/details/VehicleDetailsGallery.tsx
+++ src/modules/vehicles/details/VehicleDetailsGallery.tsx
@@ -16,21 +16,21 @@ const VehicleDetailsGallery = ({ gallery, vehicleName }: VehicleDetailsGalleryPr
         speed={500}
         plugins={[lgThumbnail, lgZoom]}
         closable
-                            showCloseIcon={true}
-                            elementClassNames='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-5 gap-2'
-                        >
-                            {gallery?.map((photo: any, index) => (
-                            <a href={photo} key={photo + index} data-src={photo}>
-                                <img
-                                src={photo}
-                                data-src={photo}
-                                alt={vehicleName}
-                                className='col-span-1 row-span-2 sm:h-[200px] md:h-[250px] w-full ease-in-out rounded-md duration-300 cursor-pointer '
-                                />
-                            </a>
-                            ))}
-                        </LightGallery>
-                        </div>
+        showCloseIcon={true}
+        elementClassNames='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-5 gap-2'
+      >
+        {gallery?.map((photo: any, index) => (
+          <a href={photo} key={photo + index} data-src={photo}>
+            <img
+              src={photo}
+              data-src={photo}
+              alt={vehicleName}
+              className='col-span-1 row-span-2 sm:h-[200px] md:h-[250px] w-full ease-in-out rounded-md duration-300 cursor-pointer '
+            />
+          </a>
+        ))}
+      </LightGallery>
+    </div>
   );
 };
 
